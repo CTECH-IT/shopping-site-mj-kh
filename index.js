ede160f4b77bbfd85fa8c81da7eb9faab5e0efb0
Kenny Harrer <7751202@isd535.org>
2021-12-07 09:43:13 -0600

    cartMODAL & item serilization

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var cart = {};
+
 window.addEventListener('load', () => {
     //deal with pre loaded items
     const items = document.querySelectorAll('galleryItem');
@@ -19,31 +21,70 @@ window.addEventListener('load', () => {
 });
 
 function seralizeGalleryItem(galleryElement) {
-    console.log(galleryElement);
     var data = {};
     data.id = galleryElement.getAttribute('id');
     data.name = galleryElement.querySelector('is').innerHTML;
     data.description = galleryElement.querySelector('about').innerHTML;
     data.price = galleryElement.querySelector('price').innerHTML;
+    data.quantity = galleryElement
+        .querySelector('div')
+        .querySelector('div')
+        .querySelector('input').value;
     return data;
 }
 
 function addToCart(galleryElement) {
-    const data = seralizeGalleryItem(galleryElement);
-    galleryElement.querySelector('button').style.display = 'none';
+    galleryElement.querySelector('button').style.display = 'none'; //hide add to cart button
+    galleryElement.appendChild(createInCartButton()); // amount changer, remove from cart
+    const data = seralizeGalleryItem(galleryElement); // get the data from this thing
+    cart[data.id] = data;
+    popCart();
 }
 
 function createInCartButton() {
     const div = document.createElement('div');
-    const left = document.createElement('button');
-    const amount = document.createElement('input');
-    amount.setAttribute('type', 'number');
-    const right = document.createElement('button');
+    const amountControls = document.createElement('div');
 
-    div.appendChild(left);
-    div.appendChild(amount);
-    div.appendChild(right);
+    const removeFromCart = document.createElement('button');
+    removeFromCart.innerText = 'Discard';
+    removeFromCart.addEventListener('click', () => {
+        div.parentElement.querySelector('button').style.display = 'block';
+        div.remove();
+        //TODO: remove from cart
+    });
+
+    const left = document.createElement('button');
+    left.innerText = '-';
+    const amount = document.createElement('input');
+    amount.value = 1;
+    const right = document.createElement('button');
+    right.innerText = '+';
+    left.addEventListener('click', () => {
+        amount.value--;
+        if (amount.value < 1) {
+            div.parentElement.querySelector('button').style.display = 'block';
+            div.remove();
+            //TODO: remove from cart
+        }
+    });
+    right.addEventListener('click', () => {
+        amount.value++;
+    });
+
+    amountControls.appendChild(left);
+    amountControls.appendChild(amount);
+    amountControls.appendChild(right);
+    div.appendChild(removeFromCart);
+    div.appendChild(amountControls);
     return div;
+}
+
+const cartMODAL = document.querySelector('cartmodal');
+
+function popCart() {
+    if (cartMODAL.style.display != 'flex') {
+        cartMODAL.style.display = 'flex';
+    }
 }
 
 function checkOut() {
